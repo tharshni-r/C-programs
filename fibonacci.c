dfda6a096afecb647d464f2a78fccb4939fb926d
tharshni-r <tharshnir27@gmail.com>
2025-09-08 11:50:11 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,a=0,b=1,c;
+    printf("enter the number of terms:");
+    scanf("%d",&n);
+    printf("fibonacci series\n");
+    for (i=1;i<=n;i++)
+    {
+        printf("%d\t",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return 0;
+  }
